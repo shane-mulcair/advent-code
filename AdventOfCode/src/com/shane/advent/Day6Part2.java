454cd2c4e1365de6022b698a11d74b0d2466a124
Shane Mulcair <jack.p.ryan@gmail.com>
2015-12-07 22:22:39 +0000

    Adding Day 6 work

---
+++ AdventOfCode/src/com/shane/advent/Day6Part2.java
@@ -0,0 +1,64 @@
+package com.shane.advent;
+
+import java.util.List;
+
+import com.shane.advent.fileaccess.OpenFileReturnListOfStrings;
+
+public class Day6Part2 {
+	static int[][] array = new int[1000][1000];
+
+	public static int lightController(String filePath) {
+		List<String> instructions = OpenFileReturnListOfStrings.openFile(filePath);
+		for (String s : instructions) {
+			if (s.contains("turn on")) {
+				getValues(s, 2, 4, 1);
+			} else if (s.contains("turn off")) {
+				getValues(s, 2, 4, 2);
+			} else {
+				getValues(s, 1, 3, 3);
+			}
+		}
+		return countLights();
+	}
+
+	private static int countLights() {
+		int total = 0;
+		for (int i = 0; i < 1000; i++) {
+			for (int j = 0; j < 1000; j++) {
+					total += array[i][j];
+			}
+		}
+		return total;
+	}
+
+	private static void getValues(String s, int i, int j, int state) {
+		String[] inputs = s.split("\\s");
+		String[] firsts = inputs[i].split(",");
+		String[] seconds = inputs[j].split(",");
+		int x1 = 0;
+		int x2 = 0;
+		int y1 = 0;
+		int y2 = 0;
+		x1 = Integer.parseInt(firsts[0]);
+		y1 = Integer.parseInt(firsts[1]);
+		x2 = Integer.parseInt(seconds[0]);
+		y2 = Integer.parseInt(seconds[1]);
+		changeLights(x1, y1, x2, y2, state);
+	}
+
+	private static void changeLights(int x1, int y1, int x2, int y2, int state) {
+		for (int i = x1; i <= x2; i++) {
+			for (int j = y1; j <= y2; j++) {
+				if (state == 1) {
+					array[i][j]++;
+				} else if (state == 2 ){
+					if(array[i][j] != 0) {
+					array[i][j]--;
+					}
+				} else {
+					array[i][j]+=2;
+				}
+			}
+		}
+	}
+}
--- AdventOfCode/src/com/shane/advent/runner/Runner.java
+++ AdventOfCode/src/com/shane/advent/runner/Runner.java
@@ -9,6 +9,10 @@ import com.shane.advent.Day4Part1;
 import com.shane.advent.Day4Part2;
 import com.shane.advent.Day5Part1;
 import com.shane.advent.Day5Part2;
+import com.shane.advent.Day6Part1;
+import com.shane.advent.Day6Part2;
+import com.shane.advent.Day7Part1;
+import com.shane.advent.tester;
 
 public class Runner {
 
@@ -21,8 +25,10 @@ public class Runner {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		System.out.println(Day5Part2.getNumNiceWords("C:\\tools\\inputfiles\\nice_words.txt"));
-		
+		//System.out.println(Day5Part2.getNumNiceWords("C:\\tools\\inputfiles\\nice_words.txt"));
+		//System.out.println(Day6Part1.lightController("C:\\tools\\inputfiles\\lights_instructions.txt"));
+		System.out.println(Day6Part2.lightController("C:\\tools\\inputfiles\\lights_instructions.txt"));
+		//Day7Part1.createList();
 	}
 	
 
